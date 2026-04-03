3c73e28534a0c5238910332f2960260dbec92d63
Antonio Tanco <antonio.tanco1999@gmail.com>
2026-04-03 14:30:47 -0400

    implemented generating unique ids for creating asset records

--- routes/inventory_router.py
+++ routes/inventory_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from db.database import datebase
 from models.assetModel import assetModel
+from utils.generateAssetID import generate_uuid
 
 inv_router = APIRouter()
 
@@ -18,4 +19,8 @@ async def read_inventory():
 @inv_router.post("/inventory/create")
 async def create_inventory_record(asset : assetModel):
 
-    return {"message": "new recorded created" , "laptop": asset}
+    id = generate_uuid()
+
+    datebase['assets'].insert_one(asset.model_dump())
+
+    return {"message": "new recorded created" , "laptop": asset, "id": id}
