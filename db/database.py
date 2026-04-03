8ce4807f63d01b0d13a11a166dd3677d8baf44cb
Antonio Tanco <antonio.tanco1999@gmail.com>
2026-04-03 13:12:03 -0400

    fixed mongoDB secrets connection in f string for uri

--- db/database.py
+++ db/database.py
@@ -4,14 +4,12 @@ import os
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
-DB_SECRET_USER = os.getenv("DB_USERNAME")
-DB_SECRET_PASS = os.getenv("DB_PASSWORD")
+db_secret_user = os.getenv("DB_USERNAME")
+db_secret_pass = os.getenv("DB_PASSWORD")
 
-print(DB_SECRET_USER)
-print(DB_SECRET_PASS)
 
-uri = f"mongodb+srv://{DB_SECRET_USER}:{DB_SECRET_PASS}@cluster0.hqwq2.mongodb.net/?appName=Cluster0"
-# Create a new client and connect to the server
+uri = f"mongodb+srv://{db_secret_user}:{db_secret_pass}@cluster0.hqwq2.mongodb.net/?appName=Cluster0"
+# Create a new client and connectd to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
@@ -24,7 +22,6 @@ try:
     datebase = client["openITAMS"]
 
     datebase.create_collection("assets")
-    
 
 except Exception as e:
     print(e)
