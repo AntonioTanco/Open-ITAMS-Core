bf1279f55de39e1b28fec9154549d8ff30b43999
Antonio Tanco <antonio.tanco1999@gmail.com>
2026-04-03 13:14:20 -0400

    implemented assetModel Dataclass in POST method for creating inventory

--- routes/inventory_router.py
+++ routes/inventory_router.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
-from db.database import client
+from db.database import datebase
+from models.assetModel import assetModel
 
 inv_router = APIRouter()
 
+# assets = {
+#     "name": "Lenovo",
+#     "model": "X1",
+#     "assignedUser": "Alex Joe"
+# }
 
 @inv_router.get("/inventory/")
 async def read_inventory():
@@ -10,6 +16,6 @@ async def read_inventory():
     return {"Inventory": None}
 
 @inv_router.post("/inventory/create")
-async def create_inventory_record():
-    db_conn = client.list_database_names()
-    return {db_conn}
+async def create_inventory_record(asset : assetModel):
+
+    return {"message": "new recorded created" , "laptop": asset}
