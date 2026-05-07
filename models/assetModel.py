4cad52889f4d363ad401570f37c1c17cf2b57cbb
Antonio Tanco <antonio.tanco1999@gmail.com>
2026-05-07 12:44:55 -0400

    extended dataclass function by creating a dataclass for API Responses

--- models/assetModel.py
+++ models/assetModel.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import uuid
 
 class assetModel(BaseModel):
     name: str
     model: str
     serialNumber: str
     assignedUser: str
+
+# Class inherts K,V from assetModel Dataclass
+class assetResponseModel(assetModel):
+    uuid: str 
